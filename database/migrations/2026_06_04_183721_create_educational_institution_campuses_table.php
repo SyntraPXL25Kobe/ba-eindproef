dc6af37d785c0f48e69b34c14dcd211b0adb6276
SyntraPXL25Kobe <Kobe.Bervoets@cursist.syntrapxl.be>
2026-06-04 20:43:20 +0200

    feat(models, migrations): add EducationalInstitutionCampus model and migration

---
+++ database/migrations/2026_06_04_183721_create_educational_institution_campuses_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('educational_institution_campuses', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->notNullable();
+            $table->foreignId('address_id')->constrained('addresses')->cascadeOnDelete();
+            $table->foreignId('educational_institution_id')->constrained('educational_institutions')->cascadeOnDelete();
+            $table->string('email')->nullable();
+            $table->string('phone')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('educational_institution_campuses');
+    }
+};
