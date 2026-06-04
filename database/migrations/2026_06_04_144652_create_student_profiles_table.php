be6481c4d1bc58c4d994cd02936d6d57ef0bd7f0
SyntraPXL25Kobe <Kobe.Bervoets@cursist.syntrapxl.be>
2026-06-04 20:19:35 +0200

    feat(migrations): update student_profiles table foreign keys

--- database/migrations/2026_06_04_144652_create_student_profiles_table.php
+++ database/migrations/2026_06_04_144652_create_student_profiles_table.php
@@ -13,10 +13,9 @@ return new class extends Migration
     {
         Schema::create('student_profiles', function (Blueprint $table) {
             $table->id();
-            $table->foreign('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
             $table->date('birth_date')->nullable();
-            $table->id('current_education_level_id')->nullable();
-            $table->foreignId('current_education_level_id')->constrained()->onDelete('set null');
+            $table->foreignId('current_education_level_id')->constrained('education_levels')->nullable()->cascadeOnDelete();
             $table->timestamps();
         });
     }
