d0ae923e6e11a907d734c44cb6abf0e6dae4e1db
guiume123 <28108746+guiume123@users.noreply.github.com>
2026-06-05 23:51:13 +0000

    fix: code style (pint/prettier/eslint)

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -37,7 +37,7 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\Filament\Widgets')
             ->widgets([
-               //
+                //
             ])
             ->middleware([
                 EncryptCookies::class,
