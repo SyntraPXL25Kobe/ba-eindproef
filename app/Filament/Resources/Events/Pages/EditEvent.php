b1edad87ad879fe26350f467bd187f48efe32ccd
guiume123 <huysmans_guillaume@hotmail.com>
2026-06-07 00:33:37 +0200

    feat(filament): redirect event edit back to view page

---
+++ app/Filament/Resources/Events/Pages/EditEvent.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Filament\Resources\Events\Pages;
+
+use App\Filament\Resources\Events\EventResource;
+use Filament\Actions\Action;
+use Filament\Actions\DeleteAction;
+use Filament\Resources\Pages\EditRecord;
+
+class EditEvent extends EditRecord
+{
+    protected static string $resource = EventResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            DeleteAction::make(),
+        ];
+    }
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('view', ['record' => $this->record]);
+    }
+
+    protected function getCancelFormAction(): Action
+    {
+        return parent::getCancelFormAction()
+            ->url($this->getResource()::getUrl('view', ['record' => $this->record]));
+    }
+}
