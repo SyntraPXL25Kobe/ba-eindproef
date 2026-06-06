2ca9593094056a1af6907f81f1b50dbe447c88fa
guiume123 <huysmans_guillaume@hotmail.com>
2026-06-06 17:07:54 +0200

    fix(models): disable timestamps on EventType model

--- app/Models/EventType.php
+++ app/Models/EventType.php
@@ -9,6 +9,8 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 #[Fillable(['name', 'code'])]
 class EventType extends Model
 {
+    public $timestamps = false;
+
     public function events(): HasMany
     {
         return $this->hasMany(Event::class);
