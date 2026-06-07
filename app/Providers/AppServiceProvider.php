00c53c2e6546193159aeee566c288456f82426a6
guiume123 <huysmans_guillaume@hotmail.com>
2026-06-07 16:01:16 +0200

    feat(filament): display admin times in Europe/Brussels timezone

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Carbon\CarbonImmutable;
+use Filament\Support\Facades\FilamentTimezone;
 use Illuminate\Support\Facades\Date;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
@@ -32,6 +33,8 @@ class AppServiceProvider extends ServiceProvider
     protected function configureDefaults(): void
     {
         Date::use(CarbonImmutable::class);
+
+        FilamentTimezone::set('Europe/Brussels');
 
         DB::prohibitDestructiveCommands(
             app()->isProduction(),
